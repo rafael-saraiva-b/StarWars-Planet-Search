119f738e734f98c6bb0e0c43578b6ca6c1e7ea94
Rafael Saraiva <rafael.saraiva.borges@gmail.com>
2022-11-22 16:32:10 -0300

    req 2

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,33 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import useData from '../services/useData';
 
 export default function Main() {
   const { isLoading, data: planets = [] } = useData('https://swapi.dev/api/planets');
+  const [search, setSearch] = useState('');
+  const [filteredByName, setFilteredByName] = useState();
   const { results } = planets;
+  useEffect(() => {
+    if (results) {
+      setFilteredByName(results.filter((result) => result.name.includes(search)));
+    }
+  }, [results, search]);
+  // const filteredByName = results.filter((result) => result.name.includes(search));
   if (isLoading) return <h1>Carregando...</h1>;
   return (
-    <table>
-      <caption>Tabela de gastos</caption>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URL</th>
-        </tr>
-      </thead>
-      <tbody>
-        { results
-          && results.map((planet, index) => (
+    <>
+      <form>
+        <label htmlFor="filtros">
+          <input
+            data-testid="name-filter"
+            type="text"
+            name="filtros"
+            placeholder="buscar"
+            value={ search }
+            onChange={ (e) => setSearch(e.target.value) }
+          />
+        </label>
+      </form>
+      <table>
+        <caption>Tabela de gastos</caption>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          { filteredByName
+          && filteredByName.map((planet, index) => (
             <tr key={ index }>
               <td>{planet.name}</td>
               <td>{planet.rotation_period}</td>
@@ -44,7 +65,8 @@ export default function Main() {
               <td>{planet.url}</td>
             </tr>
           ))}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </>
   );
 }
