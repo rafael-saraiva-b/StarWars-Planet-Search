9d432d3637b87e67c1e31c92cbdfe8ccf083b64a
Rafael Saraiva <rafael.saraiva.borges@gmail.com>
2022-11-23 15:28:42 -0300

    req 8

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import App from '../App';
+import userEvent from '@testing-library/user-event';
 
-test('I am your test', () => {
+test('testa tudo', async() => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+  const carregando = screen.getByRole('heading');
+  expect(carregando).toBeInTheDocument();
+  waitForElementToBeRemoved(carregando,{timeout:2000});
+  const table = await screen.findByRole('table');
+  expect(table).toBeInTheDocument();
+  const buscar = screen.getByTestId('name-filter')
+  expect(buscar).toBeInTheDocument();
+  userEvent.type(buscar,'oo')
+  const selectCollum = screen.getAllByRole('combobox')[0]
+  userEvent.selectOptions(selectCollum,'diameter')
+  const selectCompa = screen.getAllByRole('combobox')[1]
+  userEvent.selectOptions(selectCompa,'igual a')
+  const value = screen.getByTestId('value-filter')
+  userEvent.type(value,'120000')
+  const filtrar = screen.getByTestId('button-filter')
+  userEvent.click(filtrar)
+
 });
